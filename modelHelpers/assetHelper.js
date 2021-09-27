18a226a0a50747a43a2c49f562e5e3c111978d1a
Gaurav <crazyphoton@gmail.com>
2021-09-27 11:25:06 +0530

    removed firebase

--- modelHelpers/assetHelper.js
+++ modelHelpers/assetHelper.js
@@ -1,15 +1,5 @@
-const { realtimeDatabase } = require('../utils/firebase');
-
 let getLtp = async function (ticker) {
-  try {
-    const symbolsRef = realtimeDatabase.ref(`symbols`).child(`${ticker}::NSE`);
-    const snapshot = await symbolsRef.once('value');
-    const dataToSave = snapshot.val();
-    if (!dataToSave) return 0;
-    return dataToSave.LTP;
-  } catch (e) {
-    console.error(e);
-  }
+  return Math.random() * 1000;
   
 };
 
